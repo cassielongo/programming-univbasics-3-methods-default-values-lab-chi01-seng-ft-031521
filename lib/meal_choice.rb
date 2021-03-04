193be77e93c0b7a35d1da4d7b1043de315733714
mathematical-language-4640 <longocassie@yahoo.com>
2021-03-04 19:15:18 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++
@@ -1,6 +0,0 @@
-def meal_choice (veg1, veg2, protein = tofu)
-  puts "What a nutritious meal!"
-  puts "A plate of #{protein} with #{veg1} and #{veg2}."
-  return "A plate of tofu with carrots and string beans."
-end
-meal_choice ('carrots', 'string_beans')
